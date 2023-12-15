8e646facd6352ad2ac789f9e716648d7012c835e
ᴛᴧsʜɪᴛʜ ᴍᴧɴᴜᴋᴧ -✗ <118094017+TashithManuka2006@users.noreply.github.com>
2023-12-15 21:24:16 +0530

    Update helpers.py

--- Yukki/helpers.py
+++ Yukki/helpers.py
@@ -76,36 +76,17 @@ asyncio.create_task(auto_clean())
 
 
 RANDOM = [
-    "https://telegra.ph//file/0879fbdb307005c1fa8ab.jpg",
-    "https://telegra.ph//file/19e3a9d5c0985702497fb.jpg",
-    "https://telegra.ph//file/b5fa277081dddbddd0b12.jpg",
-    "https://telegra.ph//file/96e96245fe1afb82d0398.jpg",
-    "https://telegra.ph//file/fb140807129a3ccb60164.jpg",
-    "https://telegra.ph//file/09c9ea0e2660efae6f62a.jpg",
-    "https://telegra.ph//file/3b59b15e1914b4fa18b71.jpg",
-    "https://telegra.ph//file/efb26cc17eef6fe82d910.jpg",
-    "https://telegra.ph//file/ab4925a050e07b00f63c5.jpg",
-    "https://telegra.ph//file/d169a77fd52b46e421414.jpg",
-    "https://telegra.ph//file/dab9fc41f214f9cded1bb.jpg",
-    "https://telegra.ph//file/e05d6e4faff7497c5ae56.jpg",
-    "https://telegra.ph//file/1e54f0fff666dd53da66f.jpg",
-    "https://telegra.ph//file/18e98c60b253d4d926f5f.jpg",
-    "https://telegra.ph//file/b1f7d9702f8ea590b2e0c.jpg",
-    "https://telegra.ph//file/7bb62c8a0f399f6ee1f33.jpg",
-    "https://telegra.ph//file/dd00c759805082830b6b6.jpg",
-    "https://telegra.ph//file/3b996e3241cf93d102adc.jpg",
-    "https://telegra.ph//file/610cc4522c7d0f69e1eb8.jpg",
-    "https://telegra.ph//file/bc97b1e9bbe6d6db36984.jpg",
-    "https://telegra.ph//file/2ddf3521636d4b17df6dd.jpg",
-    "https://telegra.ph//file/72e4414f618111ea90a57.jpg",
-    "https://telegra.ph//file/a958417dcd966d341bfe2.jpg",
-    "https://telegra.ph//file/0afd9c2f70c6328a1e53a.jpg",
-    "https://telegra.ph//file/82ff887aad046c3bcc9a3.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/a7cba6e78bb63e1b4aefb.jpg",
-    "https://telegra.ph//file/f8ba75bdbb9931cbc8229.jpg",
-    "https://telegra.ph//file/07bb5f805178ec24871d3.jpg"
+    "https://telegra.ph/file/5ea1fedb75d48a8f23136.jpg",
+    "https://telegra.ph/file/7639930f1cdf333e4b583.jpg",
+    "https://telegra.ph/file/24bf4591b3714e0a0ab8e.jpg",
+    "https://telegra.ph/file/a9af59d751e1b1db8cbbd.jpg",
+    "https://telegra.ph/file/903329e79d0efba915d0f.jpg",
+    "https://telegra.ph/file/b7c64448a0ecc59e5312e.jpg",
+    "https://telegra.ph/file/66e806967268954a81dfe.jpg",
+    "https://telegra.ph/file/daf6557d448b528611fea.jpg",
+    "https://telegra.ph/file/f542cc310004d96fd241e.jpg",
+    "https://telegra.ph/file/09b2df10aea6a797a2e78.jpg",
+    "https://telegra.ph/file/fb25f957d4ad66bdbffab.jpg"
 ]
 
 
